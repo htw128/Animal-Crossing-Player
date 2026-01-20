88de504603f806eba8197c3771a6709db4773eae
Oliver <htw128@live.com>
2026-01-20 20:38:47 +0800

    [feature] ThemeSongEditor Partial

---
+++ AnimalCrossingPlayer/Assets/Scripts/OliversComputer/ACPlayer/ThemeSongEditor/NoteList.cs
@@ -0,0 +1,22 @@
+namespace OliversComputer.ACPlayer.ThemeSongEditor
+{
+    public enum NoteList
+    {
+        Rest,
+        Sustain,
+        SolLow,
+        LaLow,
+        SiLow,
+        Do,
+        Re,
+        Mi,
+        Fa,
+        Sol,
+        La,
+        Si,
+        DoHigh,
+        ReHigh,
+        MiHigh,
+        Random
+    }
+}
